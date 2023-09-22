b2ac443b4772484227965bb10923b9c8a369998d
hadi2 <hadi2@gmail.com>
2023-09-22 14:23:02 +0330

    apply chenges

--- falcon/views.py
+++ falcon/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render ,HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'falcon/base.html')
+    return render(request,'falcon/b.html')
